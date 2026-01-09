589970b1f07df0eb51309d4c067770ee5ae45480
小二 <17801063691@139.com>
2026-01-09 20:37:23 +0800

    init

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ async def main():
     # url = 'rediss://default:ASLRAAImcDFlZDM0YzQ3NTUxNTI0MjliYmVjNGFiYjBmZTExNzA2MXAxODkxMw@balanced-hagfish-8913.upstash.io:6379'
     # redis = Redis.from_url(url, decode_responses=True)
     # urls = await redis.srandmember('m3u8_urls', 1)
-    res = subprocess.run(['N_m3u8DL-RE', 'https://gmas-clena.mushroomtrack.com/hls/Lu2ofmnUsErNx6I8yzdGug/1767970860/35000/35390/35390.m3u8'], encoding='utf-8')
+    res = subprocess.run(['./N_m3u8DL-RE', 'https://gmas-clena.mushroomtrack.com/hls/Lu2ofmnUsErNx6I8yzdGug/1767970860/35000/35390/35390.m3u8'], encoding='utf-8')
     print(res)
     pass
 
